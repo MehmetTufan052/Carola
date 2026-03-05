8d479df4bb6412313d53ab17961de72cabeae222
MehmetTufan052 <tufaneser8@gmail.com>
2026-03-05 22:09:53 +0300

    ...

--- Carola.EntityLayer/Entities/Brand.cs
+++ Carola.EntityLayer/Entities/Brand.cs
@@ -9,7 +9,7 @@ namespace Carola.EntityLayer.Entities
     public class Brand
     {
 
-        public int BrandId { get; set; }
+        public int BrandId { get; set; } 
         public string BrandName { get; set; }
         public string LogoUrl { get; set; }
         public bool Status { get; set; }
